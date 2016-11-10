d1d3096b912562f1104769438e114bbab62986fd
UX303LN\tomcat <risc604@ms40.url.com.tw>
2016-11-10 17:27:16 +0800

    init created

---
+++ app/src/main/java/com/microlife/software/a24hr_mt/CreateUserProfileActivity.java
@@ -0,0 +1,41 @@
+package com.microlife.software.a24hr_mt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class CreateUserProfileActivity extends AppCompatActivity
+{
+    ImageButton saveButton;
+    EditText    etName, etBirthday;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user_profile);
+
+        saveButton = (ImageButton) findViewById(R.id.imgbtnSave);
+        etName = (EditText) findViewById(R.id.etPFName);
+        etBirthday = (EditText) findViewById(R.id.etPFBirthday);
+
+
+        saveButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
+                mainIntent.putExtra(MainActivity.EXTRAS_USER_NAME, etName.getText().toString());
+
+                startActivity(mainIntent);
+            }
+        });
+        //*/
+    }
+
+
+}
---
+++ app/src/main/java/com/microlife/software/a24hr_mt/PagerFragment.java
@@ -0,0 +1,24 @@
+package com.microlife.software.a24hr_mt;
+
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+/**
+ * Created by tomcat on 2016/9/28.
+ */
+
+//public class PagerFragment extends DialogFragment
+public class PagerFragment extends Fragment
+{
+    private final static String TAG = PagerFragment.class.getSimpleName();
+
+    public PagerFragment()
+    {
+        Log.d(TAG, "PagerFragment create.");
+    }
+    public void onPageIn()
+    {}
+
+    public void onPageOut()
+    {}
+}
